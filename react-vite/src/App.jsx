a6dbec34fdebd4e57b7ee5471be8543a96725581
Lavkush Katiyar <lavkushkatiyar@Lavkushs-MacBook-Pro.local>
2026-02-15 14:18:38 +0530

    creates intial react apps using vite and start

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>HI this is react-vite</h1>;
+}
+
+export default App;
---
+++ start_it/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1> Hello Lavkush</h1>
+  );
+}
+
+export default App;
